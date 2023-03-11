4e1fbbb4c118ec273f92f9f41d76266d24c8083a
Vikas Yadav <75388405+Vikas7302@users.noreply.github.com>
2023-03-11 18:45:32 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Collegemanagment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Course()
+        {
+
+            return View();
+
+        }
+       
+        public ActionResult Anoucement()
+        {
+            return View();
+        }
+        public ActionResult Activity()
+        {
+            return View();
+        }
+        public ActionResult Contanct()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ MYJS/ValidationJS.js
@@ -0,0 +1,7 @@
+﻿("#submit").click(function () {
+    if (document.getElementById("#firstit").value = "") {
+        alert("please feel first name");
+        return false
+    }
+
+});
